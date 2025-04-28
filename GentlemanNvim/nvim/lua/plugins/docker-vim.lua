11a8680b5ac65054ad9d004cdd72b2512b3ba1ef
Marthin Lachira <mrthnx96@gmail.com>
2025-04-28 16:56:57 -0500

    fix: remove plugin deprecated

--- GentlemanNvim/nvim/lua/plugins/docker-vim.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- dir = "/home/mrthnx/Developer/Personal/Lua/docker-vim",
-  -- name = "docker-vim",
-  -- config = function()
-  --   require("docker-vim").setup({
-  --     -- Tus opciones personalizadas aquí
-  --   })
-  -- end,
-}
